45825361f99e0a1897edafae3f21eaab2a42e8d2
chentongtong <csw0416@hotmail.com>
2017-02-20 18:06:34 +0800

    教育页面

--- pages/edit/edu/edu.js
+++ pages/edit/edu/edu.js
@@ -2,7 +2,13 @@ var editBtnGroup = require("../../../template/editBtnGroup/editBtnGroup.js")
 Page({
     data: {
         title: '教育背景',
-        nextBtn: '下一步'
+        nextBtn: '下一步',
+        eduData1: '2000-01-01',
+        eduSchool1: '',
+        eduCont1: '',
+        eduData2: '2000-01-01',
+        eduSchool2: '',
+        eduCont2: ''
     },
     save: function (e) {
         editBtnGroup.next('../exp/exp')
